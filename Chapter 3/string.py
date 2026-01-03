6faec73b1d654419de7ec6705bc320db514905b4
Sudhanshu Singh <sudhanshusingh1326@gmail.com>
2026-01-03 16:37:06 +0530

    Create string.py

---
+++ Chapter 3/string.py
@@ -0,0 +1,24 @@
+name = "Sudhanshu"
+
+email = 'sudhanshusingh@gmail.com'
+
+address='''bihar,
+           india'''
+
+# these alls are way to declear string
+
+nameshort = name[0:5]
+print(nameshort) #Sudha
+
+char = name[0]
+print(char) #S
+
+n = name[1:4:6]
+print(n)
+
+print(len(name)) #9
+
+print(name.endswith("shu")) # true
+print(name.startswith("s")) # false
+print(name.startswith("S")) #true
+
